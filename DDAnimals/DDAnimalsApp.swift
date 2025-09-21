7c3bbecf022169704a133d1a6267241763808dc5
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-09-21 23:32:34 +0900

    Initial Commit

---
+++ DDAnimals/DDAnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDAnimalsApp.swift
+//  DDAnimals
+//
+//  Created by joe on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DDAnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
